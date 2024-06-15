9e5b67b3b3da1a4dc4b95eb2896d533d20864bb3
papaskas <pavel.glugxix@gmail.com>
2024-06-15 22:01:24 +0300

    added btn listeners on main menu

--- app/src/main/java/com/papaska/tetris/MainActivity.kt
+++ app/src/main/java/com/papaska/tetris/MainActivity.kt
@@ -1,22 +1,50 @@
 package com.papaska.tetris
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvHighScore: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
         setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        hideActionBar()
+        setBtnListeners()
+    }
+
+    private fun setBtnListeners() {
+        val btnExit = findViewById<Button>(R.id.btn_exit)
+        val btnNewGame = findViewById<Button>(R.id.btn_new_game)
+        val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
+        tvHighScore = findViewById<Button>(R.id.tv_high_score)
+
+        btnExit.setOnClickListener(this::onBtnExitClick)
+        btnNewGame.setOnClickListener(this::onBtnNewGameClick)
+        btnResetScore.setOnClickListener(this::onBtnResetScoreClick)
+    }
+
+    private fun onBtnExitClick(view: View) {
+        exitProcess(0)
+    }
+
+    private fun onBtnNewGameClick(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun onBtnResetScoreClick(view: View) {
+
+    }
+
+    private fun hideActionBar() {
+        supportActionBar?.hide()
     }
 }
